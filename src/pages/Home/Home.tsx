d63eb09edd846eada59c966d496bf97e9535177d
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 22:00:14 +0300

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,33 +14,41 @@ import {
 export default function HomePage(): React.ReactElement {
   return (
     <Page title="Профиль" layout="profile">
-      <Menu>
-        <MenuGroup title="Основное">
-          <MenuButton to="/profile" icon={IconUser}>
-            Информация
-          </MenuButton>
-          <MenuButton to="/contacts" icon={IconLink}>
-            Кнопки
-          </MenuButton>
-          <MenuButton to="/design" icon={IconServices}>
-            Оформление
-          </MenuButton>
-        </MenuGroup>
-        <MenuGroup title="Инструменты">
-          <MenuButton to="/analytics" icon={IconChart}>
-            Аналитика
-          </MenuButton>
-          <MenuButton icon={IconEvents}>
-            Виджет на сайт
-          </MenuButton>
-          <MenuButton icon={IconPhoto}>
-            QR-конструктор
-          </MenuButton>
-          <MenuButton icon={IconComment}>
-            Мессенджер
-          </MenuButton>
-        </MenuGroup>
-      </Menu>
+      {[
+        {
+          slug: 'profile',
+          title: 'Профиль',
+          component: (
+            <Menu>
+              <MenuGroup title="Основное">
+                <MenuButton to="/profile" icon={IconUser}>
+                  Информация
+                </MenuButton>
+                <MenuButton to="/contacts" icon={IconLink}>
+                  Кнопки
+                </MenuButton>
+                <MenuButton to="/design" icon={IconServices}>
+                  Оформление
+                </MenuButton>
+              </MenuGroup>
+              <MenuGroup title="Инструменты">
+                <MenuButton to="/analytics" icon={IconChart}>
+                  Аналитика
+                </MenuButton>
+                <MenuButton icon={IconEvents}>
+                  Виджет на сайт
+                </MenuButton>
+                <MenuButton icon={IconPhoto}>
+                  QR-конструктор
+                </MenuButton>
+                <MenuButton icon={IconComment}>
+                  Мессенджер
+                </MenuButton>
+              </MenuGroup>
+            </Menu>
+          )
+        },
+      ]}
     </Page>
   );
 }
