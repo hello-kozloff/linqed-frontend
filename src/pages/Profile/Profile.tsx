4dcaf0b5bd9b62d28ab602bd58269eb2232b243a
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-13 00:17:44 +0300

    use input

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Page, Container, UploadImage} from '../../components';
+import {Page, Container, UploadImage, Input} from '../../components';
 
 export default function ProfilePage(): React.ReactElement {
   return (
@@ -11,6 +11,9 @@ export default function ProfilePage(): React.ReactElement {
           component: (
             <Container>
               <UploadImage url="http://fetbiko.ru/public/img/avatar.png" />
+              <Input label="Имя" defaultValue="Даниил Фетисов" />
+              <Input label="Описание" defaultValue="Product Designer at Linqed.me" />
+              <Input label="Адрес страницы" defaultValue="linqed.me/fetchlab" />
             </Container>
           )
         },
