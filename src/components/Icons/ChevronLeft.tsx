01573666f94e64dabdd28a99c51fdd3ffacb7a6b
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 20:47:31 +0300

    add icon

---
+++ src/components/Icons/ChevronLeft.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconChevronLeft = (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M17.7071 1.29289C18.0976 1.68342 18.0976 2.31658 17.7071 2.70711L8.41421 12L17.7071 21.2929C18.0976 21.6834 18.0976 22.3166 17.7071 22.7071C17.3166 23.0976 16.6834 23.0976 16.2929 22.7071L7 13.4142C6.21895 12.6332 6.21895 11.3668 7 10.5858L16.2929 1.29289C16.6834 0.902369 17.3166 0.902369 17.7071 1.29289Z" fill="#000B19"/>
+  </svg>
+);
+
+export default IconChevronLeft;
