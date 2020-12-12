6ad370c1cc84d909f7ce0f8b8f303d2ceef76795
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 23:07:38 +0300

    create header component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Menu } from './Menu/Menu';
 export { default as Page } from './Page/Page';
 export { default as Container } from './Container/Container';
 export { default as Avatar } from './Avatar/Avatar';
+export { default as Header } from './Header/Header';
