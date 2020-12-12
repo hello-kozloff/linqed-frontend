7f49a41c92595402dcff76d3bb792406e1618410
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 04:56:08 +0300

    upgrade props

--- src/components/Menu/modules/Button/types.ts
+++ src/components/Menu/modules/Button/types.ts
@@ -1,9 +1,11 @@
 import { ReactNode } from 'react';
+import { LinkProps } from 'react-router-dom';
 
 /**
  * The menu button module props interface.
  */
 export interface IMenuButtonProps {
+  to: LinkProps['to'];
   icon: ReactNode;
   children: string;
 }
