019c9247c52f4e3f197a628110be79413a3e74e1
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 21:19:31 +0300

    add comments

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -4,8 +4,17 @@ import { PageHeader } from "./modules";
 import { IPageProps } from './types';
 import './styles.scss';
 
+/**
+ * The component class generator.
+ */
 const page = block('page');
 
+/**
+ * The page component.
+ *
+ * @param props
+ * @constructor
+ */
 export default function Page(props: IPageProps): React.ReactElement {
   return (
     <div className={page({ layout: props.layout })}>
--- src/components/Page/types.ts
+++ src/components/Page/types.ts
@@ -1,6 +1,9 @@
 import {ReactNode, ReactNodeArray} from "react";
 import {IPageHeaderProps} from "./modules/Header/types";
 
+/**
+ * The page props interface.
+ */
 export interface IPageProps {
   title: IPageHeaderProps['title'];
   layout: string;
