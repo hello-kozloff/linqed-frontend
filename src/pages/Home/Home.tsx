0f7d7cd732d54b67a806af9abdc83510780612f0
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 21:17:54 +0300

    upgrade render

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from '../../components';
+import { Page, Menu } from '../../components';
 import { MenuGroup, MenuButton } from "../../components/Menu/modules";
 import {
   IconUser,
@@ -13,32 +13,34 @@ import {
 
 export default function HomePage(): React.ReactElement {
   return (
-    <Menu>
-      <MenuGroup title="Основное">
-        <MenuButton to="/profile" icon={IconUser}>
-          Информация
-        </MenuButton>
-        <MenuButton to="/contacts" icon={IconLink}>
-          Кнопки
-        </MenuButton>
-        <MenuButton to="/design" icon={IconServices}>
-          Оформление
-        </MenuButton>
-      </MenuGroup>
-      <MenuGroup title="Инструменты">
-        <MenuButton to="/analytics" icon={IconChart}>
-          Аналитика
-        </MenuButton>
-        <MenuButton icon={IconEvents}>
-          Виджет на сайт
-        </MenuButton>
-        <MenuButton icon={IconPhoto}>
-          QR-конструктор
-        </MenuButton>
-        <MenuButton icon={IconComment}>
-          Мессенджер
-        </MenuButton>
-      </MenuGroup>
-    </Menu>
+    <Page title="Профиль" layout="profile">
+      <Menu>
+        <MenuGroup title="Основное">
+          <MenuButton to="/profile" icon={IconUser}>
+            Информация
+          </MenuButton>
+          <MenuButton to="/contacts" icon={IconLink}>
+            Кнопки
+          </MenuButton>
+          <MenuButton to="/design" icon={IconServices}>
+            Оформление
+          </MenuButton>
+        </MenuGroup>
+        <MenuGroup title="Инструменты">
+          <MenuButton to="/analytics" icon={IconChart}>
+            Аналитика
+          </MenuButton>
+          <MenuButton icon={IconEvents}>
+            Виджет на сайт
+          </MenuButton>
+          <MenuButton icon={IconPhoto}>
+            QR-конструктор
+          </MenuButton>
+          <MenuButton icon={IconComment}>
+            Мессенджер
+          </MenuButton>
+        </MenuGroup>
+      </Menu>
+    </Page>
   );
 }
