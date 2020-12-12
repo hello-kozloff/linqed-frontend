9188d3e1c63d1fbe7099a00830065ae566af0e45
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-12 20:30:56 +0300

    create icons

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,7 @@
+export { default as IconUser } from './User';
+export { default as IconLink } from './Link';
+export { default as IconServices } from './Services';
+export { default as IconChart } from './Chart';
+export { default as IconEvents } from './Events';
+export { default as IconPhoto } from './Photo';
+export { default as IconComment } from './Comment';
