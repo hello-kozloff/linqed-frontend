667331610da48e2bc2b49f959c257fae70b6cf71
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-13 18:03:48 +0300

    fix

--- src/pages/View/View.tsx
+++ src/pages/View/View.tsx
@@ -43,8 +43,8 @@ export default function ViewPage(): React.ReactElement {
               <ViewLink emoji="🚀" title="Блог в Telegram" />
             </div>
             <span className={viewPage('copyright')}>
-            Создай такую же страницу на <a href="https://www.linqed.me" target="_blank" rel="noreferrer">linqed.me</a> 🚀
-          </span>
+              Создай такую же страницу на <a href="https://www.linqed.me" target="_blank" rel="noreferrer">linqed.me</a> 🚀
+            </span>
           </div>
           <div className={viewPage('button')}>
             <Button to="/">
