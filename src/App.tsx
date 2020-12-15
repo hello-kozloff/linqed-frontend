35acc5ae2adfe2ca070dfebb30c52734aedb1d75
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-15 05:06:23 +0300

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import { HomePage, ProfilePage, ButtonsPage, ViewPage } from "./pages";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename="/">
       <Switch>
         <Route exact path="/view" component={ViewPage} />
         <Route exact path="/profile" component={ProfilePage} />
         <Route exact path="/buttons" component={ButtonsPage} />
         <Route exact path="/" component={HomePage} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
